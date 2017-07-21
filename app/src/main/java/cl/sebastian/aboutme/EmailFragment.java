3923b80acb9290da15ba220f3db37a2cd5b54c1a
Sebastian <smenap@gmail.com>
2017-07-21 16:53:08 -0400

    First commit

---
+++ app/src/main/java/cl/sebastian/aboutme/EmailFragment.java
@@ -0,0 +1,50 @@
+package cl.sebastian.aboutme;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EmailFragment extends Fragment {
+
+
+    public EmailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_email, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button contactBtn = (Button) view.findViewById(R.id.contactBtn);
+        contactBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Funcionó", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Button button = (Button) getActivity().findViewById(R.id.contactBtn);
+    }
+}
