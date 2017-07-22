bb5dcb15dbe47ef6fe0291cc8954e47c58ded232
sebamena <smenap@gmail.com>
2017-07-22 11:00:11 -0400

    cuarto commit

--- app/src/main/java/cl/sebastian/aboutme/EmailFragment.java
+++ app/src/main/java/cl/sebastian/aboutme/EmailFragment.java
@@ -4,9 +4,7 @@ package cl.sebastian.aboutme;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -22,16 +20,21 @@ public class EmailFragment extends Fragment {
     }
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_email, container, false);
-    }
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_email, container, false);
+//    }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+
+
+
 
         Button contactBtn = (Button) view.findViewById(R.id.contactBtn);
         contactBtn.setOnClickListener(new View.OnClickListener() {
